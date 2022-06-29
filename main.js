b25923cf5b2ded5975039aa6fe1cf12cc0e00988
Poorvi1010 <91541629+Poorvi1010@users.noreply.github.com>
2022-06-29 17:34:45 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,12 @@
 song1=""
 song2=""
+
+LeftWrist_x=0;
+LeftWrist_y=0;
+
+RightWrist_x=0;
+RightWrist_y=0
+
 function preload()
 {
 song1=loadSound("music.mp3");
@@ -13,6 +20,26 @@ canvas.center();
 
 video=createCapture(VIDEO);
 video.hide();
+
+poseNet=ml5.poseNet(video,modelLoaded);
+poseNet.on("pose",gotPoses);
+}
+
+function modelLoaded()
+{
+    console.log("Pose Net is Initialized");
+}
+
+function gotPoses(results)
+{
+    if(results.length>0)
+    {
+        console.log(results);
+        LeftWrist_x= results[0].pose.leftWrist.x;
+        LeftWrist_y= results[0].pose.leftWrist.y;
+        RightWrist_x=results[0].pose.rightWrist.x;
+        RightWrist_y=results[0].pose.rightWrist.y;
+    }
 }
 
 function draw()
